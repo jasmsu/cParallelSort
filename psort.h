2b8c1f926feb9b1c9699cee2bc13e9606d28fba1
Jason Su <jasmsu@gmail.com>
2013-04-16 01:08:48 -0500

    Fixed whitespace
    Fixed unaligned tabs

--- psort.h
+++ psort.h
@@ -16,43 +16,43 @@ namespace ParallelSort
     template <class T>
     void psort(vector<T>* array);
 
-	/**
-	* quickSort - implementation of parallel sort using quicksort
-	* @param array - the structure to sort
-	* @param start - index of start of array section to sort
-	* @param end - index of end of array section to sort
-	*/
+    /**
+    * quickSort - implementation of parallel sort using quicksort
+    * @param array - the structure to sort
+    * @param start - index of start of array section to sort
+    * @param end - index of end of array section to sort
+    */
     template <class T>
     void quickSort(vector<T> * array, int start, int end);
 	
-	/**
-	 * quickSortSections - unused implementation of quicksort 
-	 * parallelization using omp sections instead of tasks
-	 * @param array - the structure to sort
-	 * @param start - index of start of array section to sort
-	 * @param end - index of end of array section to sort
-	 */
+    /**
+     * quickSortSections - unused implementation of quicksort 
+     * parallelization using omp sections instead of tasks
+     * @param array - the structure to sort
+     * @param start - index of start of array section to sort
+     * @param end - index of end of array section to sort
+     */
     template<class T>
     void quickSortSection(vector<T> * array, int start, int end);
 
-	/**
-	 * quickSortSerial - serial quicksort
-	 * @param array - the structure to sort
-	 * @param start - index of start of array section to sort
-	 * @param end - index of end of array section to sort
-	 */
+    /**
+     * quickSortSerial - serial quicksort
+     * @param array - the structure to sort
+     * @param start - index of start of array section to sort
+     * @param end - index of end of array section to sort
+     */
     template<class T>
     void quickSortSerial(vector<T> * array, int start, int end);
 
-	/**
-	 * insertionSort - Unused insertion sort that could be used to
-	 * optimize sorts of small arrays of items, as insertion sort is
-	 * faster on smaller sized arrays, however testing indicated that
-	 * sort did not come out faster, so it was not used.
-	 * @param array - the structure to sorts
-	 * @param start - index of start of array section to sort
-	 * @param end - index of end of array section to sort
-	 */
+    /**
+     * insertionSort - Unused insertion sort that could be used to
+     * optimize sorts of small arrays of items, as insertion sort is
+     * faster on smaller sized arrays, however testing indicated that
+     * sort did not come out faster, so it was not used.
+     * @param array - the structure to sorts
+     * @param start - index of start of array section to sort
+     * @param end - index of end of array section to sort
+     */
     template <class T>
     void insertionSort(vector<T> * array, int start, int end);
 	
